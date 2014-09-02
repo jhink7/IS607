d7a652d225d9486e031ade707dfb3a46489833d3
jhink7 <jhink7@gmail.com>
2014-09-02 09:59:22 -0400

    Quiz 1 done

--- quiz1/quiz1.R
+++ quiz1/quiz1.R
@@ -29,3 +29,182 @@ v["Last"]
 v["Position"]
 v["Team"]
 v["Number"]
+
+#8
+
+c1 <- 1:10
+c2 <- c("first", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th")
+c3 <- factor(1:5)
+
+df <- data.frame(C1 = c1, C2 = c2, C3 = c3)
+df
+
+#9
+c1new = c(11)
+c3new = factor(c1new)
+r1 = data.frame(C1 = c1new, C2 ="11th", C3 = factor(c1new))
+df = rbind(df, r1)
+
+#10
+temps <- read.table(file = "temperatures.csv", header = TRUE, sep = ",")
+
+#11
+temps <- read.table(file = "C:/Users/Justin/Documents/temperatures.txt", header = TRUE, sep = "\t")
+
+#12
+temps <- read.table(file = "http://www.temprepo/citytemps.txt", header = TRUE, sep = "|")
+
+#13
+fact = 1
+for(i in 1:12)
+{
+  fact = fact * i
+}
+
+fact
+
+#14
+balance = 1500
+for(i in 1:72)
+{
+  balance = balance * 1.032
+}
+
+balance
+
+#15
+vect = 1:20
+sum = 0
+count = 1
+for(i in 1:20)
+{
+  if(count == 3)
+  {
+    sum = sum + i
+    count = 1
+  }
+  else
+  {
+    count = count + 1
+  }
+}
+
+sum
+
+#16
+sum = 0
+for(i in 1:10)
+{
+  sum = sum + 2 ^ i
+}
+
+sum
+
+#17
+sum = 0
+i = 1
+while (i <= 10)
+{
+  sum = sum + 2 ^ i
+  i = i + 1
+}
+
+sum
+
+#18
+vect = 1:10
+temp = 2 ^ vect
+sum(temp)
+
+#19
+vect = c(20)
+
+count = 1
+for(i in 21:50)
+{
+  if(count == 5)
+  {
+    vect = append(vect, i)
+    count = 1
+  }
+  else
+  {
+    count = count + 1
+  }
+}
+
+vect
+
+#20
+testWord = "example"
+cVect = c(testWord)
+
+for(i in 1:9)
+{
+  cVect = append(cVect, testWord)
+}
+
+cVect
+
+#21
+y <- getquadratic(1,2,3)
+
+getquadratic <- function(a,b,c)
+{
+  x = -100:100
+  return (a * x ^ 2 + b * x + c)
+}
+
+y
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
