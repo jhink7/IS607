5c90d6fdf09df8a691d4955793e82aebab2345a8
jhink7 <jhink7@gmail.com>
2014-09-22 21:28:24 -0400

    Project 2 Started

---
+++ Project 2/proj2.R
@@ -0,0 +1,45 @@
+setwd("C:/Users/Justin/Documents/GitHub/IS607/Project 2")
+
+library(ggplot2)
+library(plyr)
+library(data.table)
+library(dplyr) 
+
+masterHitters = read.csv("Hitters.csv") 
+
+hitterTable <- data.table(masterHitters)
+
+#### Offense By Year ####
+yearlyOffenseProxyTable <- hitterTable[, sum(R) / sum(PA), by = Season]
+yearlyOffenseProxy <- data.frame(Season = yearlyOffenseProxyTable[[1]], Offense = yearlyOffenseProxyTable[[2]])
+
+p1 <- ggplot(yearlyOffenseProxy, aes(x= Season, y = Offense)) + geom_bar(stat = "identity")
+p1 + annotate("text", x = 2000, y =0.16,  label = "Steroids") + annotate("text", x = 1969, y =0.16,  label = "Mound Lowered") + annotate("text", x = 1908, y =0.16,  label = "Dead Ball Era")
+
+modernOffenseProxy <- subset(yearlyOffenseProxy, Season > 1980)
+p2 <- ggplot(modernOffenseProxy, aes(x= Season, y = Offense)) + geom_bar(stat = "identity")
+p2 + annotate("text", x = 2000, y =0.16,  label = "Steroids")
+
+#### Offense By Team ####
+teamOffenseProxyTable <- hitterTable[, sum(R) / sum(PA), by = Team]
+setnames(teamOffenseProxyTable, c("Team","Offense") )
+
+teamOffenseProxyTable$Team3 <- reorder(teamOffenseProxyTable$Team, teamOffenseProxyTable$Offense)
+
+p2 <- ggplot(teamOffenseProxyTable, aes(y=Offense))
+p2 + geom_bar(aes(x=Team3), data=teamOffenseProxyTable, stat="identity")
+
+#### Modern Offense Per Team ####
+modernHitterTable = subset(hitterTable, Season > 1980)
+teamOffenseProxyTable <- modernHitterTable[, sum(R) / sum(PA), by = Team]
+setnames(teamOffenseProxyTable, c("Team","Offense") )
+
+teamOffenseProxyTable$Team3 <- reorder(teamOffenseProxyTable$Team, teamOffenseProxyTable$Offense)
+
+teamOffenseProxyTable <- arrange(teamOffenseProxyTable,desc(Offense))
+
+p2 <- ggplot(teamOffenseProxyTable, aes(y=Offense))
+p2 + geom_bar(aes(x=Team3), data=teamOffenseProxyTable, stat="identity")
+
+print(teamOffenseProxyTable)
+
